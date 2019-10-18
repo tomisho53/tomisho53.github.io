9258025e7fcd11ef52f25724a8427501e9b90130
Hajime Egawa <hajime_egawa@mail.ryoyu.co.jp>
2019-10-18 15:50:52 +0900

    Initial commit

--- js/main.js
+++ js/main.js
@@ -20,6 +20,7 @@
       // 初回起動時に2番目に実行される
       // 初回以降は1番目に実行される
       console.log('ServiceWorker registration successful with scope: ', registration.scope);
+      
       // navigator.serviceWorker.ready -> ServiceWorker が activeになるまでコードの実行を遅らせる
       navigator.serviceWorker.ready
               .then((registration) => {
@@ -28,64 +29,65 @@
                 // 初回以降は2番目に実行される
                 console.log('ServiceWorker ready complate!!');
 
-                // クリックイベント登録
-                // sync1
-                document.getElementById('sync1').addEventListener('click', () => {
-                  // 保存が終わったら、↓を呼ぶ
-                  registration.sync.register('sync1')
-                    .then(() => {
-                      // sync登録に成功した場合の処理
-                      console.log('sync1 registerd');
+                // // クリックイベント登録
+                // // sync1
+                // document.getElementById('sync1').addEventListener('click', () => {
+                //   // 保存が終わったら、↓を呼ぶ
+                //   registration.sync.register('sync1')
+                //     .then(() => {
+                //       // sync登録に成功した場合の処理
+                //       console.log('sync1 registerd');
 
-                      // // Ajax呼出（JQuery）
-                      // let request = {
-                      //   objectId  : 1,
-                      //   latitude  : 2,
-                      //   longitude : 3
-                      // };
-                      // $.ajax({
-                      //   type      : 'post',
-                      //   url       : "https://mbaas.api.nifcloud.com/2013-09-01/classes/Test?X-NCMB-Application-Key=9042d4c028a7dc1c1802685fa5f9944addf4dca4a403671f11ccfdcec8e2b9b8&X-NCMB-Signature=Q/fT/wvXvinnatgjgBoOrwuV0uYbeyFe53Q2d+k0JI4=",
-                      //   data      : JSON.stringify(request),
-                      //   dataType  : 'json',
-                      //   success: function(data){
-                      //     console.log('success');
-                      //     console.log(data);
-                      //   },
-                      //   error: function(data){
-                      //     console.log('error');
-                      //   }
-                      // });
+                //       // // Ajax呼出（JQuery）
+                //       // let request = {
+                //       //   objectId  : 1,
+                //       //   latitude  : 2,
+                //       //   longitude : 3
+                //       // };
+                //       // $.ajax({
+                //       //   type      : 'post',
+                //       //   url       : "https://mbaas.api.nifcloud.com/2013-09-01/classes/Test?X-NCMB-Application-Key=9042d4c028a7dc1c1802685fa5f9944addf4dca4a403671f11ccfdcec8e2b9b8&X-NCMB-Signature=Q/fT/wvXvinnatgjgBoOrwuV0uYbeyFe53Q2d+k0JI4=",
+                //       //   data      : JSON.stringify(request),
+                //       //   dataType  : 'json',
+                //       //   success: function(data){
+                //       //     console.log('success');
+                //       //     console.log(data);
+                //       //   },
+                //       //   error: function(data){
+                //       //     console.log('error');
+                //       //   }
+                //       // });
                       
-                      // 接続状態設定
-                      if (navigator.onLine){
-                        document.getElementById('syncstate').innerText = 'online';
-                      }else{
-                        document.getElementById('syncstate').innerText = 'offline';
-                      }
-                    })
-                    .catch(console.error.bind(console));
-                  }, false);
+                //       // 接続状態設定
+                //       if (navigator.onLine){
+                //         document.getElementById('syncstate').innerText = 'online';
+                //       }else{
+                //         document.getElementById('syncstate').innerText = 'offline';
+                //       }
+                //     })
+                //     .catch(console.error.bind(console));
+                //   }, false);
+                // })
 
-                // sync2
-                document.getElementById('sync2').addEventListener('click', () => {
-                  // 保存が終わったら、↓を呼ぶ
-                  registration.sync.register('sync2')
-                    .then(() => {
-                      // sync登録に成功した場合の処理
-                      console.log('sync2 registerd');
+                // // sync2
+                // document.getElementById('sync2').addEventListener('click', () => {
+                //   // 保存が終わったら、↓を呼ぶ
+                //   registration.sync.register('sync2')
+                //     .then(() => {
+                //       // sync登録に成功した場合の処理
+                //       console.log('sync2 registerd');
 
-                      // 接続状態設定
-                      if (navigator.onLine){
-                        document.getElementById('syncstate').innerText = 'online';
-                      }else{
-                        document.getElementById('syncstate').innerText = 'offline';
-                      }
-                    })
-                    .catch(console.error.bind(console));
-                  }, false);
-                })
-                .catch(console.error.bind(console));
+                //       // 接続状態設定
+                //       if (navigator.onLine){
+                //         document.getElementById('syncstate').innerText = 'online';
+                //       }else{
+                //         document.getElementById('syncstate').innerText = 'offline';
+                //       }
+                //     })
+                //     .catch(console.error.bind(console));
+                //   }, false);
+                // })
+                // .catch(console.error.bind(console));
 
                 // TimerStart
                 document.getElementById('timerstart').addEventListener('click', () => {
@@ -93,12 +95,11 @@
                 })
                 .catch(console.error.bind(console));
 
-                // TimerStop
-                document.getElementById('timerstart').addEventListener('click', () => {
+                // // TimerStop
+                // document.getElementById('timerstart').addEventListener('click', () => {
                    
-                })
-                .catch(console.error.bind(console));
-
+                // })
+                // .catch(console.error.bind(console));
 
     }).catch(function(err) {
       console.log('ServiceWorker registration failed: ', err);
