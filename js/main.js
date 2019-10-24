7642b698fa11601975fe95dcaf37feed957cde4b
Hajime Egawa <hajime_egawa@mail.ryoyu.co.jp>
2019-10-24 17:06:59 +0900

    Initial commit

--- js/main.js
+++ js/main.js
@@ -41,8 +41,8 @@
                   }else{
                     document.getElementById('syncstate').innerText = 'offline';
                   }
-                  console.log(registration.sync);
-                  document.getElementById('log').innerText = registration.sync;
+                  console.log(registration);
+                  document.getElementById('log').innerText = registration;
 
                   // 保存が終わったら、↓を呼ぶ
                   registration.sync.register('sync1')
