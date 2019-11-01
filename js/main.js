405805832d1aeb29377342b6d2475eeae878c3da
Hajime Egawa <hajime_egawa@mail.ryoyu.co.jp>
2019-11-01 15:18:47 +0900

    Initial commit

--- js/main.js
+++ js/main.js
@@ -133,7 +133,7 @@
         info: null
       }
     },
-    mouted() {
+    mounted() {
       axios
         .get('https://api.coindesk.com/v1/bpi/currentprice.json')
         .then(response => (this.info = response))
