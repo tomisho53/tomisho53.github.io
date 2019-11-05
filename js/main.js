573c8f11b06997082ffdff9f1cb77743ec7be294
Hajime Egawa <hajime_egawa@mail.ryoyu.co.jp>
2019-11-05 13:20:04 +0900

    Initial commit

--- js/main.js
+++ js/main.js
@@ -154,4 +154,84 @@
 
   console.log(vm);
 
+  // Geolocation API
+  // var watchID;
+  if (navigator.geolocation) {
+
+    // 現在の位置情報を取得
+    // watchID = navigator.geolocation.watchPosition(
+    navigator.geolocation.getCurrentPosition(
+
+      // 位置情報の取得を成功した場合
+      function (pos) {
+
+        // coords.latitude 現在位置の緯度
+        // coords.longitude　現在位置の経度。
+        // coords.altitude 現在位置の高度。メートル単位。
+        // coords.accuracy 取得した緯度、経度の精度。メートル単位。
+        // coords.altitudeAccuracy 取得した高度の精度。メートル単位。
+        // coords.heading 方角。0〜360の角度で表す。0が北、90が東、180が南、270が西。
+        // coords.speed　速度。メートル/秒数。位置情報を追跡する場合に使用。
+
+        var location = "<li>" + "緯度：" + pos.coords.latitude + "</li>";
+        location += "<li>" + "経度：" + pos.coords.longitude + "</li>";
+        location += "<li>" + "高度：" + pos.coords.altitude + "</li>";
+        location += "<li>" + "緯度・経度の精度：" + pos.coords.accuracy + "</li>";
+        location += "<li>" + "高度の精度：" + pos.coords.altitudeAccuracy + "</li>";
+        location += "<li>" + "方角：" + pos.coords.heading + "</li>";
+        location += "<li>" + "速度：" + pos.coords.speed + "</li>";
+
+        document.getElementById("location").innerHTML = location;
+
+        // //Google Mapsで住所を取得
+        // var geocoder = new google.maps.Geocoder();
+        // latlng = new google.maps.LatLng(pos.coords.latitude, pos.coords.longitude);
+        // geocoder.geocode({'latLng': latlng}, function(results, status) {
+        //   if (status == google.maps.GeocoderStatus.OK) {
+        //     console.log(results);
+        //     // document.getElementById('address').innerHTML = results[0].formatted_address;
+        //     // console.log("1:" + location);
+        //     location += "<li>" + "住所：" + results[0].formatted_address + "</li>";
+        //     document.getElementById("location").innerHTML = location;
+        //     // console.log("2:" + location);
+        //   }
+        //   else {
+        //     alert("エラー" + status);
+        //   }
+        // });
+        // // console.log("3:" + location);
+        // // document.getElementById("location").innerHTML = location;
+
+        // console.log('Sta : 位置情報取得成功処理');
+        // // 4. Google Maps APIの位置情報オブジェクトを生成
+        // var latitude = pos.coords.latitude;   // 緯度
+        // var longitude = pos.coords.longitude; // 経度
+        // console.log('緯度：' + latitude + '／経度：' + longitude)
+        // var latlng = new google.maps.LatLng(latitude, longitude);
+
+        // // 5. 地図を表示
+        // map = new google.maps.Map(document.getElementById("mapCanvas"), {
+        //   zoom: 14,       // ズームレベル
+        //   center: latlng 	// 中心地を指定
+        // });
+
+        // // 6. マーカーを置く
+        // var marker = new google.maps.Marker({position: latlng, map: map});
+        // console.log('End : 位置情報取得成功処理');
+
+      },
+
+      null,
+      { enableHighAccuracy: true }
+
+    );
+
+  } else {
+    window.alert("本ブラウザではGeolocationが使えません");
+  }
+
+  // function clearWatchPosition() {
+  //   navigator.geolocation.clearWatch(watchID);
+  // }
+
 }
