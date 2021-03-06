d07a4ed6b8d75794d7a591a2b639b1de24eb03fc
hajime egawa <hajime.egawa.erc@gmail.com>
2021-03-06 20:56:31 +0900

    Initial commit

--- js/htmlLv2.js
+++ js/htmlLv2.js
@@ -2,9 +2,6 @@ const device_orientation_z = document.getElementById("device_orientation_z");
 const device_orientation_x = document.getElementById("device_orientation_x");
 const device_orientation_y = document.getElementById("device_orientation_y");
 
-window.addEventListener("deviceorientation",(e) => {c
-})
-
 const requestDeviceOrientationPermission = () => {
   if (
     DeviceOrientationEvent &&
@@ -35,6 +32,34 @@ const requestDeviceOrientationPermission = () => {
 }
 
 // ボタンクリックでrequestDeviceOrientationPermission実行
-const startButton = document.getElementById("start-button");
-console.log(startButton);
-startButton.addEventListener('click', requestDeviceOrientationPermission, false);
+const do_startButton = document.getElementById("device-orientation-start-button");
+do_startButton.addEventListener('click', requestDeviceOrientationPermission, false);
+
+
+const requestDeviceMotionPermission = () => {
+  if (
+    DeviceMotionEvent &&
+    typeof DeviceMotionEvent.requestPermission === 'function'
+  ) {
+    // iOS 13+ の Safari
+    // 許可を取得
+    DeviceMotionEvent.requestPermission()
+    .then(permissionState => {
+      if (permissionState === 'granted') {
+        // 許可を得られた場合、devicemotionをイベントリスナーに追加
+        window.addEventListener('devicemotion', e => {
+          // devicemotionのイベント処理
+        })
+      } else {
+        // 許可を得られなかった場合の処理
+      }
+    })
+    .catch(console.error) // https通信でない場合などで許可を取得できなかった場合
+  } else {
+    // 上記以外のブラウザ
+  }
+}
+
+// ボタンクリックでrequestDeviceMotionPermission実行
+const dm_startButton = document.getElementById("start-button")
+dm_startButton.addEventListener('click', requestDeviceMotionPermission, false)
